b218ac1272ef7e850d34a1e39e17745f47049ef4
Pascal Wittler <git@pascalwittler.de>
2023-12-29 15:43:04 +0100

    Prepare data processing per year from data subfolders

--- public/data.php
+++ public/data.php
@@ -5,5 +5,14 @@ $data_folder_paths = array_filter(glob("{$data_parent_folder_path}/*"), 'is_dir'
 
 $data = [];
 
+foreach ($data_folder_paths as $data_folder_path) {
+    $year = basename($data_folder_path);
+
+    $data[$year] = [
+        'stops' => [],
+        'dimensions' => [],
+    ];
+}
+
 header('Content-Type: application/json');
 echo json_encode($data);
