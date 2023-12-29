3b9a2ea985ddcfa1d0fd25b5abcf1bf28106a055
Pascal Wittler <git@pascalwittler.de>
2023-12-29 08:22:08 +0100

    Prepare PHP data output

--- public/data.php
+++ public/data.php
@@ -2,3 +2,8 @@
 
 $data_parent_folder_path = realpath(__DIR__ . '/../data') . '/';
 $data_folder_paths = array_filter(glob("{$data_parent_folder_path}/*"), 'is_dir');
+
+$data = [];
+
+header('Content-Type: application/json');
+echo json_encode($data);
