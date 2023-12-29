0c3dc308fde6f91cb37476215b07d8e9b9ab1e85
Pascal Wittler <git@pascalwittler.de>
2023-12-29 15:48:30 +0100

    Add specific csv file processing function

--- public/data.php
+++ public/data.php
@@ -7,6 +7,29 @@ function yield_next_line($file_resource)
     }
 }
 
+function get_csv_data(string $file_path): array
+{
+    $csv_file_resource = fopen($file_path, 'r');
+
+    $keys = [];
+    $csv_data = [];
+
+    foreach (yield_next_line($csv_file_resource) as $line) {
+        if (empty($keys)) {
+            $keys = str_getcsv($line);
+            continue;
+        }
+
+        if (str_contains($line, 'de:08212')) {
+            $csv_data[] = array_combine($keys, str_getcsv($line));
+        }
+    }
+
+    fclose($csv_file_resource);
+
+    return $csv_data;
+}
+
 $data_parent_folder_path = realpath(__DIR__ . '/../data') . '/';
 $data_folder_paths = array_filter(glob("{$data_parent_folder_path}/*"), 'is_dir');
 
