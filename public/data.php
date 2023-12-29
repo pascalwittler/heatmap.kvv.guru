4e2e821d3f80ea593cdd5ab37178e5e65565c631
Pascal Wittler <git@pascalwittler.de>
2023-12-29 15:45:50 +0100

    Add generic file streaming generator function

--- public/data.php
+++ public/data.php
@@ -1,5 +1,12 @@
 <?php
 
+function yield_next_line($file_resource)
+{
+    while (($line = fgets($file_resource)) !== false) {
+        yield $line;
+    }
+}
+
 $data_parent_folder_path = realpath(__DIR__ . '/../data') . '/';
 $data_folder_paths = array_filter(glob("{$data_parent_folder_path}/*"), 'is_dir');
 
